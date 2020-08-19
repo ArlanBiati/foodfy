ed7cf6bb004cef8f2bd99451284de4ca7f57f3d3
Arlan <arlangustavo@hotmail.com>
2020-08-18 23:18:00 -0300

    Chore: Refactoring pages

---
+++ public/scripts.js
@@ -0,0 +1,8 @@
+// click captura ID e joga na rota via params.
+
+function capturaId(click_id) {
+    window.location.href = `/recipe/${click_id}`
+}
+
+// função botao mostrar / esconder conteudo receita
+
