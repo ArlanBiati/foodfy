a4a48d3dbdd0588dcf712bb8ce5b14261f2ae568
Arlan <arlangustavo@hotmail.com>
2020-09-01 18:03:40 -0300

    Feat: Inclusão da rota de criação de receitas e apresentação

--- recipes.js
+++ recipes.js
@@ -1,34 +1,60 @@
-const fs = require("fs")
-const data = require("./data.json")
+const fs = require('fs')
+const data = require('./data.json')
 
-exports.index = function(req, res) {
-    return res.render("page-admin/listing", { recipes })
+exports.index = function (req, res) {
+  return res.render('page-admin/listing', { recipes: data.recipes })
 }
 
-exports.create = function(req, res) {
-    return res.render("page-admin/create")    
+exports.create = function (req, res) {
+  return res.render('page-admin/create', { recipes: data.recipes.values })
+  // erro de direcionamento
 }
 
-exports.show = function(req, res) {
-    const recipeId = req.params.id
+exports.show = function (req, res) {
+  const { id } = req.params
 
-    const recipe = recipes.find(e => e.id == recipeId )
+  const foundRecipe = data.recipes.find((recipe) => {
+    return recipe.id == id
+  })
+  if (!foundRecipe) {
+    return res.send('Receita não encontrada')
+  }
 
-    return res.render("page-admin/detail", { recipe }) 
+  return res.render('page-admin/detail', { recipe: foundRecipe })
 }
 
-exports.edit = function(req, res) {
-    
+exports.edit = function (req, res) {}
+
+exports.post = function (req, res) {
+  const keys = Object.keys(req.body)
+
+  for (key of keys) {
+    if (req.body[key] == '') {
+      return res.send('Por favor, preencha todos os campos.')
+    }
+  }
+
+  let { image, title, ingredients, preparations, information } = req.body
+
+  const id = Number(data.recipes.length + 1)
+
+  data.recipes.push({
+    id,
+    image,
+    title,
+    ingredients,
+    preparations,
+    information,
+  })
+
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), function (err) {
+    if (err) {
+      return res.send('Erro de escrita do arquivo')
+    }
+    return res.redirect('/admin/recipes')
+  })
 }
 
-exports.post = function(req, res) {
-    
-}
+exports.put = function (req, res) {}
 
-exports.put = function(req, res) {
-    
-}
-
-exports.delete = function(req, res) {
-    
-}
+exports.delete = function (req, res) {}
--- routes.js
+++ routes.js
@@ -1,62 +1,35 @@
-const express = require("express")
+const express = require('express')
 const routes = express.Router()
+const recipes = require('./recipes')
 
-// const recipes = require("./recipes")
-
-const recipes = require("./data")
-
-
-routes.get("/", function(req, res) {
-    return res.render("page-client/home", { recipes })
+routes.get('/', function (req, res) {
+  return res.render('page-client/home', { recipes })
 })
 
-routes.get("/about", function(req, res) {
-    return res.render("page-client/about")
+routes.get('/about', function (req, res) {
+  return res.render('page-client/about')
 })
 
-routes.get("/recipes/:id", function(req, res) {
+routes.get('/recipes/:id', function (req, res) {
+  const recipeId = req.params.id
 
-    const recipeId = req.params.id
+  const recipe = recipes.find((e) => e.id == recipeId)
 
-    const recipe = recipes.find(e => e.id == recipeId )
-
-    return res.render("page-client/recipe", { recipe })
-
+  return res.render('page-client/recipe', { recipe })
 })
 
+routes.get('/admin/recipes', recipes.index) // Mostrar a lista de receitas - Ok
 
-// routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
-routes.get("/admin/recipes", function(req, res) {
-    return res.render("page-admin/listing", { recipes })
-})
+routes.get('/admin/recipes/:id', recipes.show) // Exibir detalhes de uma receita - Ok
 
+routes.get('/admin/recipes/:id/edit', recipes.edit) // Mostrar formulário de edição de receita
 
-// routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
-routes.get("/admin/recipes/:id", function(req, res) {
+routes.get('/admin/recipes/create', recipes.create) // Mostrar formulário de nova receita
 
-    const recipeId = req.params.id
+routes.post('/admin/recipes', recipes.post) // Cadastrar nova receita - Ok
 
-    const recipe = recipes.find(e => e.id == recipeId )
+routes.put('/admin/recipes', recipes.put) // Editar uma receita
 
-    return res.render("page-admin/detail", { recipe })
-})
-
-
-// routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
-routes.get("/admin/recipes/:id/edit", function(req, res) {
-    return res.render("page-admin/edit", { recipes })
-})
-
-
-// routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
-routes.get("/admin/recipes/create", function(req, res) {
-    return res.render("page-admin/create")
-})
-
-
-
-// routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
-// routes.put("/admin/recipes", recipes.put); // Editar uma receita
-// routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
+routes.delete('/admin/recipes', recipes.delete) // Deletar uma receita
 
 module.exports = routes
